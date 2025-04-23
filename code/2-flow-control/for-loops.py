36adb35e948d75535e070c84f068ba6fa1b58912
Clare Gibson <clarelgibson@gmail.com>
2025-04-23 07:39:40 +0100

    Lessons 2 and 3

---
+++ code/2-flow-control/for-loops.py
@@ -0,0 +1,10 @@
+# Print a statement 5 times
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+# Add up all the numbers from 0 to 100
+total = 0
+for num in range(101):
+    total += num
+print(total)
---
+++ code/3-functions/built-in-functions.py
@@ -0,0 +1,6 @@
+import sys
+
+# The code below will print 'Hello' and then exit the program before printing 'Goodbye'.
+print('Hello')
+sys.exit()
+print('Goodbye')
